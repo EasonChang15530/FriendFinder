ede13e5e202e95c69883a6de9d2fb517dbdb8450
eason <eason15530@gmail.com>
2019-11-22 10:01:42 -0500

    add server routes

---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+// Uses the path package to get the correct file path for html
+var path = require("path");
+
+// gets requests and shows user an HTML page
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+};
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
+// EXPRESS CONFIGURATION
+// Sets up the basic properties for express server
+var express = require('express');
+
+// Creates an express server
+var app = express();
+
+// Sets an initial port.
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sets router
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Sets listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
